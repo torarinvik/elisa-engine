35fb3504982a95240777bc3774eb242093ea30d0
torarinvik <torarinvik@gmail.com>
2026-09-25 19:09:06 +0200

    Negotiate FSR support per graphics device

--- native/render_scene_abi.cpp
+++ native/render_scene_abi.cpp
@@ -159,7 +159,7 @@ struct RenderSceneService {
     float camera_near_clip = DEFAULT_CAMERA_NEAR_CLIP;
     float camera_far_clip = DEFAULT_CAMERA_FAR_CLIP;
     bool perspective_camera = false;
-    int32_t shadow_quality = 1; float sun_shadow_receiver_bias = 0.0f;
+    int32_t shadow_quality = 1; float sun_shadow_receiver_bias = 0.0f; probe::UpscalerCapabilities upscaler_capabilities{};
     float eye[3] = {0.0f, 15.0f, 0.0f};
     float target[3] = {0.0f, 0.0f, 0.0f};
     float up[3] = {0.0f, 0.0f, -1.0f};
@@ -398,7 +398,7 @@ void reset_unlocked(RenderSceneService& state) {
     state.camera_near_clip = DEFAULT_CAMERA_NEAR_CLIP;
     state.camera_far_clip = DEFAULT_CAMERA_FAR_CLIP;
     state.perspective_camera = false;
-    state.shadow_quality = 1; state.sun_shadow_receiver_bias = 0.0f;
+    state.shadow_quality = 1; state.sun_shadow_receiver_bias = 0.0f; state.upscaler_capabilities = {};
     state.initialized = false;
 }
 void on_application_shutdown(void* context) {
